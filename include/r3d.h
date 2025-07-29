c87a9e0cf6e6a2c5c4083515ea744d61ba6ab081
Grunthos <66932486+grunthon@users.noreply.github.com>
2025-07-29 06:33:13 -0400

    revert uneeded skybox description change

--- include/r3d.h
+++ include/r3d.h
@@ -316,7 +316,7 @@ typedef unsigned int R3D_Light;
  * @brief Structure representing a skybox and its related textures for lighting.
  *
  * This structure contains textures used for rendering a skybox, as well as
- * precomputed lighting textures and data used for image-based lighting (IBL).
+ * precomputed lighting textures used for image-based lighting (IBL).
  */
 typedef struct R3D_Skybox {
     TextureCubemap cubemap;  ///< The skybox cubemap texture for the background and reflections.
